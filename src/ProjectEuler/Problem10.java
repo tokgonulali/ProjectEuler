d447988fe03d69c12156def2cf1a3e2626d1627a
tokgonulali <45030755+tokgonulali@users.noreply.github.com>
2018-11-14 11:35:56 +0300

    Update Problem10.java
    asd

--- src/ProjectEuler/Problem10.java
+++ src/ProjectEuler/Problem10.java
@@ -4,7 +4,10 @@ public class Problem10 {
 
 	public static void main(String[] args) {
 		long sum = 2;
-	    outerloop:
+		
+
+		
+	 outerloop:
 	    for(int i = 3; i < 2000000; i+=2){
 	        for(int j = 2; j <= i; j++){
 	            if(i != j && i % j == 0){
